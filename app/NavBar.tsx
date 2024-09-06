fe2d4e28c92dc457cb7e99cfc485a6ed2d89913d
Venkatesan R S <96675735+Tyzen-10@users.noreply.github.com>
2024-09-06 17:48:37 +0530

    prisma basic setup and initial migration
    added .env to gitignore

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,20 +1,35 @@
-import Link from 'next/link'
-import React from 'react'
+"use client"; //can only access browserAPIs in client components.
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
 import { AiFillBug } from "react-icons/ai";
 
 const NavBar = () => {
+  const currPath = usePathname(); //what happens if i declare this outside component.
   const links = [
-    {label:"Dashboard", href: "/"},
-    {label:"Issues", href:"/issues"},
-  ]
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
   return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-        <Link href="/"><AiFillBug></AiFillBug></Link>
-        <ul className='flex space-x-6'>
-          {links.map((link)=><Link key={link.href} href={link.href} className='text-zinc-500 hover:text-zinc-800 transition-colors'>{link.label}</Link>)}
-        </ul>
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <AiFillBug></AiFillBug>
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            href={link.href}
+            className={`${
+              link.href === currPath ? "text-green-900" : "text-green-500"
+            } hover:text-green-800 transition-colors`}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
