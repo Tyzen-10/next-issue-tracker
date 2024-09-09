7814e676a9800ce38246ea333686d8aef4ba3f0c
Venkatesan R S <96675735+Tyzen-10@users.noreply.github.com>
2024-09-10 01:18:12 +0530

    New issue page

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}><Theme><NavBar></NavBar><main>{children}</main></Theme></body>
+      <body className={inter.className}><Theme><NavBar></NavBar><main className='p-5'>{children}</main></Theme></body>
     </html>
   )
 }
