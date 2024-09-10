a8678ac28a2db6868afbab47b531d7e36e6c25c6
Venkatesan R S <96675735+Tyzen-10@users.noreply.github.com>
2024-09-10 11:04:09 +0530

    Handling Form Submission.
    Makes Entry in DB.
    Redirects back to issues page.

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,15 +2,31 @@
 import { Button, TextArea, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
 import React from "react";
+import { useRouter } from "next/navigation"; //only this one works with app router.
+
+//is this IssueForm interface, necessary. Becoz TS seemed to work without this.
+interface IssueForm {
+    title: string;
+    description: string;
+}
 
 const NewIssuePage = () => {
+    const router = useRouter(); 
+    const {register, control, handleSubmit} = useForm<IssueForm>();
+    // console.log(register('title'));
+    //handleSubmit (reactHookForm) -> returns a function that will be used onSubmit.s
   return (
-    <div className="max-w-xl space-y-3">
-      <TextField.Root placeholder="Title"></TextField.Root>
-      <SimpleMDE placeholder="Description" />
+    <form className="max-w-xl space-y-3" onSubmit={handleSubmit(async (data)=> {
+        await axios.post('/api/issues', data); //axios returns a promise.
+        router.push('/issues'); //go to issues page.
+    })}>
+      <TextField.Root placeholder="Title" {...register("title")}></TextField.Root>
+      <Controller render={({field})=><SimpleMDE placeholder="Description" {...field}></SimpleMDE>} name="description" control={control}></Controller>
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 
